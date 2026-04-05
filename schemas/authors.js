abb48b90448f7e484d60b297a0962672d941378e
thuypy1209 <huymn944@gmail.com>
2026-04-05 12:43:06 +0700

    update authors

--- schemas/authors.js
+++ schemas/authors.js
@@ -1,9 +1,18 @@
-let mongoose = require('mongoose');
-
+let mongoose = require("mongoose");
 let authorSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    biography: { type: String },
-    birthYear: { type: Number }
-}, { timestamps: true });
-
-module.exports = mongoose.model('Author', authorSchema);
+  name: { 
+    type: String, 
+    required: true },
+  bio: { 
+    type: String, 
+    default: "" },
+  slug: { 
+    type: String, 
+    unique: true, 
+    sparse: true },
+  isDeleted: { 
+    type: Boolean, 
+    default: false }
+}, { 
+    timestamps: true });
+module.exports = mongoose.model("author", authorSchema);
