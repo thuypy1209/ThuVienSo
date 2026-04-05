e58c2d7db420a10b2025a21040d49239d9ec7caa
thuypy1209 <huymn944@gmail.com>
2026-04-05 17:33:50 +0700

    Create cart

--- schemas/borrowRecords.js
+++ schemas/borrowRecords.js
@@ -1,11 +1,28 @@
-const mongoose = require('mongoose');
+let mongoose = require('mongoose');
 
-const borrowRecordSchema = new mongoose.Schema({
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    book: { type: mongoose.Schema.Types.ObjectId, ref: 'Book', required: true },
-    borrowDate: { type: Date, default: Date.now }, // Mặc định là ngày hôm nay
-    dueDate: { type: Date, required: true }, // Ngày hạn trả
-    status: { type: String, enum: ['Đang mượn', 'Đã trả', 'Quá hạn'], default: 'Đang mượn' }
-}, { timestamps: true });
+let borrowRecordSchema = new mongoose.Schema({
+    user: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'User',
+         required: true 
+        },
+    book: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'Book', required: true 
+    },
+    borrowDate: { 
+        type: Date, 
+        default: Date.now 
+    }, 
+    dueDate: { 
+        type: Date, 
+        required: true 
+    }, 
+    status: { 
+        type: String, enum: ['Đang mượn', 'Đã trả', 'Quá hạn'], 
+        default: 'Đang mượn' 
+    }
+}, {
+     timestamps: true });
 
 module.exports = mongoose.model('BorrowRecord', borrowRecordSchema);
